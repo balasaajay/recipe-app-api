ff99e85e8f67317d9d75d2717034135f18442a5c
Ajay Balasa <ajay.balasa@amd.com>
2022-12-25 12:35:52 -0800

    Terraform for creating bastion ec2 instance

--- deploy/main.tf
+++ deploy/main.tf
@@ -3,16 +3,16 @@
 terraform {
   required_version = ">= 0.12"
   backend "s3" {
-    bucket = "recipe-app-api-tfstate-aj"
-    key = "recipe-app.tfstate"
-    region = "us-east-1"
-    encrypt = true
+    bucket         = "recipe-app-api-tfstate-aj"
+    key            = "recipe-app.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
     dynamodb_table = "recipe-app-api-tfstate-lock"
   }
 }
 
 provider "aws" {
-    region = "us-east-1"
-    version = "~>2.54.0"
+  region  = "us-east-1"
+  version = "~>2.54.0"
 }
 
